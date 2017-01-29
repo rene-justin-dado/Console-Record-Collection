a866317f4c5e4cc749ff3be7ae94153df6997004
Justin Dado <justindado@yahoo.com>
2017-01-29 17:51:36 +1300

    Add project files.

---
+++ PracticeApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
